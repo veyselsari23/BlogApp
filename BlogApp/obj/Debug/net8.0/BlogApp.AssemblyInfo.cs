1a307533b0f8800ab388c3643e174f7c8f8bd534
veyselsari23 <veyselsari23@gmail.com>
2024-10-23 23:30:16 +0300

    json cozulde

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b03ec5be5efd6584a9f5ecf4856053019331751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c40a160edc7207c0bd59009b5ce31791b836db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
