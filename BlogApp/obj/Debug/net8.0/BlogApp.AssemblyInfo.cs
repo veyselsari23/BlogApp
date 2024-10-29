a7372b5d6341ce6835f56e1e375e70ee9cd0adc3
veyselsari23 <veyselsari23@gmail.com>
2024-10-29 23:21:17 +0300

    uu

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a307533b0f8800ab388c3643e174f7c8f8bd534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d632fbe403693536a3d825db195af5b1f2a704")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
